9967db9bd147afb957bc294dd51bf99fc0bbd545
HAndmind4 <jaredsarwar@gmail.com>
2026-01-29 16:18:29 +0600

    Change DB host and add connection options
    Updated database connection settings to use a specific host and added connection options.

--- db.js
+++ db.js
@@ -2,10 +2,12 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+  host: "db4free.net",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 5
 });
 
 db.connect(err => {
