274095eefff582f91211521c4040a9e023b77ae5
HAndmind4 <jaredsarwar@gmail.com>
2026-01-29 16:31:47 +0600

    Refactor database connection to use connection pool
    Switched from createConnection to createPool for better connection management.

--- db.js
+++ db.js
@@ -1,18 +1,16 @@
 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+// USE A POOL (required for DB4Free)
+const db = mysql.createPool({
   host: "db4free.net",
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: 3306,
   waitForConnections: true,
-  connectionLimit: 5
-});
-
-db.connect(err => {
-    
-    console.log("MySQL Connected");
+  connectionLimit: 5,
+  queueLimit: 0
 });
 
 module.exports = db;
